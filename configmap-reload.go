067a20eb962ae065213c839e9f786c7e1b5e87b1
l.aminov <l.aminov@tinkoff.ru>
2019-01-10 18:47:46 +0500

    Add telemetry

--- configmap-reload.go
+++ configmap-reload.go
@@ -7,14 +7,58 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	fsnotify "github.com/fsnotify/fsnotify"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var volumeDirs volumeDirsFlag
-var webhook webhookFlag
-var webhookMethod = flag.String("webhook-method", "POST", "the HTTP method url to use to send the webhook")
-var webhookStatusCode = flag.Int("webhook-status-code", 200, "the HTTP status code indicating successful triggering of reload")
+const namespace = "configmap_reload"
+
+var (
+	volumeDirs        volumeDirsFlag
+	webhook           webhookFlag
+	webhookMethod     = flag.String("webhook-method", "POST", "the HTTP method url to use to send the webhook")
+	webhookStatusCode = flag.Int("webhook-status-code", 200, "the HTTP status code indicating successful triggering of reload")
+	listenAddress     = flag.String("web.listen-address", ":9355", "Address to listen on for web interface and telemetry.")
+	metricPath        = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
+
+	requestDuration = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "last_request_duration_seconds",
+		Help:      "Duration of last webhook request",
+	}, []string{"webhook"})
+	successReloads = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "success_reloads_total",
+		Help:      "Total success reload calls",
+	}, []string{"webhook"})
+	requestErrorsByReason = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "request_errors_total",
+		Help:      "Total errors while requesting reload by reason",
+	}, []string{"webhook", "reason"})
+	watcherErrors = prometheus.NewCounter(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "watcher_errors_total",
+		Help:      "Total errors while requesting reload by reason",
+	})
+	requestsByStatusCode = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "requests_total",
+		Help:      "Total requests by response status code",
+	}, []string{"webhook", "status_code"})
+)
+
+func init() {
+	prometheus.MustRegister(requestDuration)
+	prometheus.MustRegister(successReloads)
+	prometheus.MustRegister(requestErrorsByReason)
+	prometheus.MustRegister(watcherErrors)
+	prometheus.MustRegister(requestsByStatusCode)
+}
 
 func main() {
 	flag.Var(&volumeDirs, "volume-dir", "the config map volume directory to watch for updates; may be used multiple times")
@@ -35,6 +79,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	go func() {
+		err := serverMetrics(*listenAddress, *metricPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		log.Fatal(err)
@@ -46,30 +97,37 @@ func main() {
 		for {
 			select {
 			case event := <-watcher.Events:
-				if event.Op&fsnotify.Create == fsnotify.Create {
-					if filepath.Base(event.Name) == "..data" {
-						log.Println("config map updated")
-						for _, h := range webhook {
-							req, err := http.NewRequest(*webhookMethod, h, nil)
-							if err != nil {
-								log.Println("error:", err)
-								continue
-							}
-							resp, err := http.DefaultClient.Do(req)
-							if err != nil {
-								log.Println("error:", err)
-								continue
-							}
-							resp.Body.Close()
-							if resp.StatusCode != *webhookStatusCode {
-								log.Println("error:", "Received response code", resp.StatusCode, ", expected", *webhookStatusCode)
-								continue
-							}
-							log.Println("successfully triggered reload")
-						}
+				if !isValidEvent(event) {
+					continue
+				}
+				log.Println("config map updated")
+				for _, h := range webhook {
+					begun := time.Now()
+					req, err := http.NewRequest(*webhookMethod, h, nil)
+					if err != nil {
+						requestErrorsByReason.WithLabelValues(h, "client_request_create").Inc()
+						log.Println("error:", err)
+						continue
 					}
+					resp, err := http.DefaultClient.Do(req)
+					if err != nil {
+						requestErrorsByReason.WithLabelValues(h, "client_request_do").Inc()
+						log.Println("error:", err)
+						continue
+					}
+					resp.Body.Close()
+					requestsByStatusCode.WithLabelValues(h, strconv.Itoa(resp.StatusCode)).Inc()
+					if resp.StatusCode != *webhookStatusCode {
+						requestErrorsByReason.WithLabelValues(h, "client_response").Inc()
+						log.Println("error:", "Received response code", resp.StatusCode, ", expected", *webhookStatusCode)
+						continue
+					}
+					requestDuration.WithLabelValues(h).Set(time.Since(begun).Seconds())
+					successReloads.WithLabelValues(h).Inc()
+					log.Println("successfully triggered reload")
 				}
 			case err := <-watcher.Errors:
+				watcherErrors.Inc()
 				log.Println("error:", err)
 			}
 		}
@@ -83,6 +141,16 @@ func main() {
 		}
 	}
 	<-done
+}
+
+func isValidEvent(event fsnotify.Event) bool {
+	if event.Op&fsnotify.Create != fsnotify.Create {
+		return false
+	}
+	if filepath.Base(event.Name) != "..data" {
+		return false
+	}
+	return true
 }
 
 type volumeDirsFlag []string
@@ -104,4 +172,20 @@ func (v *webhookFlag) Set(value string) error {
 
 func (v *webhookFlag) String() string {
 	return fmt.Sprint(*v)
+}
+
+func serverMetrics(listenAddress, metricsPath string) error {
+	http.Handle(metricsPath, promhttp.Handler())
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+			<html>
+			<head><title>ConfigMap Reload Metrics</title></head>
+			<body>
+			<h1>ConfigMap Reload</h1>
+			<p><a href='` + metricsPath + `'>Metrics</a></p>
+			</body>
+			</html>
+		`))
+	})
+	return http.ListenAndServe(listenAddress, nil)
 }
