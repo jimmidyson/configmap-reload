386d51d482b455591f27db607f53b9f1a20b37c5
Mike Gilbode <mike.gilbode@oracle.com>
2020-06-25 16:17:53 -0400

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,18 @@
 #!/bin/sh
 
-make cross docker BASEIMAGE=oraclelinux:7.8@sha256:3b8917d800082c4823c4011c3fc4b098e1d50537838e863718516f401621cb93
+set -x
+
+BASEIMAGE="oraclelinux:7.8@sha256:3b8917d800082c4823c4011c3fc4b098e1d50537838e863718516f401621cb93"
+
+if [ -z "${DOCKER_IMAGE_NAME}" ] ; then
+    echo "Environment variable DOCKER_IAMGE_NAME not set"
+    exit 1
+fi
+if [ -z "${DOCKER_IMAGE_TAG}" ] ; then
+    echo "Environment variable DOCKER_IAMGE_TAG not set"
+    exit 1
+fi
+
+make out/configmap-reload-linux-amd64
+
+docker build --build-arg BASEIMAGE="${BASEIMAGE}" -t "${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}"
